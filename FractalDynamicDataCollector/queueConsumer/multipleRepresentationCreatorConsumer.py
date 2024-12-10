f6fad6431ddf288c59543d81a9cb68cc724c2aa6
jperdek <perdek.jakub@gmail.com>
2024-12-10 21:01:42 +0100

    Updating Docker compose abnout worker

--- FractalDynamicDataCollector/queueConsumer/multipleRepresentationCreatorConsumer.py
+++ FractalDynamicDataCollector/queueConsumer/multipleRepresentationCreatorConsumer.py
@@ -4,9 +4,10 @@ import os
 
 import pika
 
-from queueConsumer.global_worker_configuration import DataRepresentationsClient
+from global_worker_configuration import DataRepresentationsClient
 
 logger = logging.getLogger(__name__)
+
 
 def callback_func(channel, method, properties, body):
     task_configuration = json.loads(body)
@@ -15,14 +16,15 @@ def callback_func(channel, method, properties, body):
     project_id = task_configuration["projectId"]
     evolution_iteration = task_configuration["evolutionIteration"]
 
-    DataRepresentationsClient.create_all_representations(evolved_spl_path, evolved_spl_script_path, evolution_iteration, project_id, logger)
+    DataRepresentationsClient.create_all_representations(evolved_spl_path, evolved_spl_script_path, evolution_iteration,
+                                                         project_id, logger)
 
 
 if __name__ == "__main__":
-    credentials = pika.PlainCredentials(os.getenv("PROCESSING_QUEUE_USERNAME", "splManager"),
-                                        os.getenv("PROCESSING_QUEUE_PASSWORD", "splManager"))
+    credentials = pika.PlainCredentials(os.getenv("CONSUMER_USER_NAME", "consumerUser"),
+                                        os.getenv("CONSUMER_USER_PASSWORD", "consumerUser"))
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=os.getenv("RABBIT_MQ_HOST", "localhost"),
+        pika.ConnectionParameters(host=os.getenv("RABBIT_MQ_HOST", "docker.host.internal"), port=5672,
                                   credentials=credentials))
 
     channel = connection.channel()
---
+++ rabbitMQ/init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# FROM https://stackoverflow.com/questions/30747469/how-to-add-initial-users-when-starting-a-rabbitmq-docker-container
+# Ensure the nodename doesn't change, e.g. if docker restarts.
+# Important because rabbitmq stores data per node name (or 'IP')
+echo 'NODENAME=rabbit@localhost' > /etc/rabbitmq/rabbitmq-env.conf
+
+# Create Rabbitmq user
+(rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
+echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2
+rabbitmq-server $@
